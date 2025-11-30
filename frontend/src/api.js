1902bac610a678af7660c6cc6bc8add9d3b42f97
abdurazakm <abdulkem5472gmail.com>
2025-11-30 03:42:50 +0300

    local server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'https://fast-delivery-4gog.onrender.com/api',
-  // baseURL: 'http://localhost:4800/api',
+  // baseURL: 'https://fast-delivery-4gog.onrender.com/api',
+  baseURL: "http://localhost:4800/api",
 });
 
 // Request interceptor to add token if it exists
 API.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token'); // get token from localStorage
+    const token = localStorage.getItem("token"); // get token from localStorage
 
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`; // attach token if available
+      config.headers["Authorization"] = `Bearer ${token}`; // attach token if available
     }
     // If no token, just send request without Authorization header
     return config;
